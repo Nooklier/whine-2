dfd27dc0617cd96ea2eba673610d548b62eb3f83
Nooklier <muenseepromn@gmail.com>
2024-04-27 13:24:53 -0700

    time off request routes done

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,7 @@ from .models import db, User
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .api.paid_time_off_routes import pto_routes  
+from .api.time_off_request_routes import time_off_routes
 from .seeds import seed_commands
 from .config import Config
 
@@ -30,6 +31,7 @@ app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(pto_routes, url_prefix='/api/pto')
+app.register_blueprint(time_off_routes, url_prefix='/api/timeoff')
 db.init_app(app)
 Migrate(app, db)
 
