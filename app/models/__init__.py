384b398707aecf2b307907a4396bea6513e869f7
Nooklier <muenseepromn@gmail.com>
2024-04-27 01:29:17 -0700

    pto model and migration added

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,5 @@
 from .db import db
 from .user import User, UserRole
 from .time_off_request import TimeOffRequest
+from .paid_time_off import PaidTimeOff
 from .db import environment, SCHEMA
---
+++ app/models/paid_time_off.py
@@ -0,0 +1,22 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+class PaidTimeOff(db.Model):
+    __tablename__ = 'paid_time_offs'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    total_hours = db.Column(db.Integer, default=0)
+    used_hours = db.Column(db.Integer, default=0)
+    remaining_hours = db.Column(db.Integer, default=0)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'total_hours': self.total_hours,
+            'used_hours': self.used_hours,
+            'remaining_hours': self.remaining_hours
+        }
