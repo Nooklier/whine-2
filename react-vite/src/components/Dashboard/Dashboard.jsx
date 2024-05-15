3039469fe8c37898544bdccd1ae3e4adb8d3374d
Nooklier <muenseepromn@gmail.com>
2024-05-15 08:24:06 +0700

    left side of dashboard is almost done

--- react-vite/src/components/Dashboard/Dashboard.jsx
+++ react-vite/src/components/Dashboard/Dashboard.jsx
@@ -4,6 +4,12 @@ import { fetchShifts } from '../../redux/shift';
 import "./Dashboard.css";
 import { NavLink } from "react-router-dom";
 import { thunkLogout } from "../../redux/session"
+import searchIcon from '../../../photos/search-icon.png'
+import scheduleIcon from '../../../photos/schedule-icon.png'
+import timeOffIcon from '../../../photos/time-off-icon.png'
+import ptoIcon from '../../../photos/pto-icon.png'
+import settingIcon from '../../../photos/setting-icon.png'
+import signOutIcon from '../../../photos/sign-out-icon.png'
 
 function Dashboard() {
     const user = useSelector(state => state.session.user);
@@ -24,41 +30,64 @@ function Dashboard() {
 
     return (
         <div className="dashboard-container">
-            <div className="menu-container">
-                <div>Menu</div>
-                <div>{user.first_name} {user.last_name}</div>
-                <ul>
-                    <NavLink to='/shift'>SCHEDULE</NavLink>
-                    <li>TIME OFF REQUEST</li>
-                    <li>PTO</li>
-                </ul>
-                <div>
-                    <li>SETTINGS</li>
-                    <li>SIGN OUT</li>
-                </div>
-            </div>
+            <div className="dashboard-inside-container">
 
-            <div className="dashboard-container-inside">
-                <div>Dashboard</div>
+                <div className="dashboard-left-container">
+                    <div>
+                        <div className="dashboard-menu">Menu</div>
+                        <div className="search-container">
+                            <input className='search-bar'type="search" placeholder="SEARCH"></input>
+                            <img className='search-icon'src={searchIcon} alt="search"></img>
+                        </div>
+                        <div className="name">{user.first_name} {user.last_name}</div>
+                        <div className="menu-container">
+                            <div className="nav-link-container">
+                                <img className='navlink-icon' src={scheduleIcon} alt="schedule"></img>
+                                <NavLink className='nav-link' to='/shift'>SCHEDULE</NavLink>
+                            </div>
+                            <div className="nav-link-container">
+                                <img className='navlink-icon' src={timeOffIcon} alt="time off request"></img>
+                                <NavLink className='nav-link' >TIME OFF REQUEST</NavLink>
+                            </div>
+                            <div className="nav-link-container">
+                                <img className='navlink-icon' src={ptoIcon} alt="pto"></img>
+                                <NavLink className='nav-link' >PTO</NavLink>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="bottom-container">
+                        <div className="left-bottom-container">
+                            <img className='navlink-icon' src={settingIcon}></img>
+                            <div className='setting' onClick={() => alert('Feature coming soon!')}>SETTINGS</div>
+                        </div>
+                        <div className="left-bottom-container">
+                            <img className='navlink-icon' src={signOutIcon}></img>
+                            <NavLink style={{textDecoration: 'none'}} onClick={handleLogout} to='/'>SIGN OUT</NavLink>
+                        </div>
+                    </div>
+                </div>
+
                 <div>
-                    <div>Upcoming</div>
+                    <div>Dashboard</div>
+                    <div>
+                        <div>Upcoming</div>
+                        <ul>
+                            {shifts.map(shift => (
+                                <ul key={shift.id}>{shift.shift_date}: {shift.shift_start} - {shift.shift_end}</ul>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+
+                <div>
+                    <div>NOTICEBOARD</div>
                     <ul>
-                        {shifts.map(shift => (
-                            <ul key={shift.id}>{shift.shift_date}: {shift.shift_start} - {shift.shift_end}</ul>
-                        ))}
+                        <li>MANDATORY TRAINING</li>
+                        <li>TEAM MEMBER SIGNATURE REMINDER</li>
                     </ul>
                 </div>
             </div>
-
-            <div className="noticeboard-container">
-                <div>NOTICEBOARD</div>
-                <ul>
-                    <li>MANDATORY TRAINING</li>
-                    <li>TEAM MEMBER SIGNATURE REMINDER</li>
-                </ul>
-                <button onClick={handleFetchShifts}>Fetch Shifts</button>
-            </div>
-            <button onClick={handleLogout}>LOG OUT</button>
         </div>
     );
 }
