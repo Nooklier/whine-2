2e4358a91318b85489ba003b70ce43c7cb0190db
Nooklier <muenseepromn@gmail.com>
2024-06-17 15:24:00 -0700

    praying

--- app/models/user.py
+++ app/models/user.py
@@ -2,6 +2,7 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from enum import Enum
+from sqlalchemy.dialects.postgresql import ENUM
 from sqlalchemy.orm import relationship
 
 class UserRole(Enum):
